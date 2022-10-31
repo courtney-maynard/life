2de0ced1d6aa1659dd2a6d0a3ee4d1d1ce9b7f22
Courtney Maynard <cbmaynard@wm.edu>
2022-10-30 21:19:21 -0400

    Complete Part 4 of Part 1, add 4,.c and 4.out

---
+++ ASSIGNMENT_08/Part_I/4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+  
+int new_array[100] = {24};
+
+/*function declaration*/
+
+int practice_func();
+
+int main() {
+   printf("Hello, World!");
+
+   int practiceval = practice_func();
+
+   return 0;
+}
+
+int practice_func(){
+	int second_array[100];
+	int third_array[100] = {24};
+	return 0;
+
+}
