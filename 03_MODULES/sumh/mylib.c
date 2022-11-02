4845a1d6442ea92a12e1d291c5189e03df4f351b
Courtney Maynard <cbmaynard@wm.edu>
2022-11-02 17:47:59 -0400

    Add 03_MODULES from class nov 2

---
+++ 03_MODULES/sumh/mylib.c
@@ -0,0 +1,11 @@
+long cumulative_sum(int N)
+{
+ long accumulator = 0;
+
+ for (int n = 1; n <= N; n++)
+ {
+	accumulator = accumulator + n;
+ }
+
+ return accumulator;
+}
---
+++ 03_MODULES/sumh/mylib.h
@@ -0,0 +1,5 @@
+#ifndef mylib_h__
+#define my lib_h__
+long(cumulative_sum(int N));
+#endif
+
