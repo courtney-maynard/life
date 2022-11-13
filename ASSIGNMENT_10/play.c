e44d4d54420379aafefa79da95b47890c4024072
Courtney Maynard <cbmaynard@wm.edu>
2022-11-13 12:12:40 -0500

    Add function two

--- ASSIGNMENT_10/play.c
+++ ASSIGNMENT_10/play.c
@@ -6,3 +6,11 @@ void one (char ca[24] )
 	printf(" addr of (ca[1]) = %#x \n", &(ca[1]));
 	printf(" ++ca = %#x \n\n", ++ca);
 }
+
+void two (char *pa )
+{
+	printf(" addr of pointer param = %#x \n", &pa);
+	printf(" addr of (pa[0]) = %#x \n", &(pa[0]));
+	printf(" addr of (pa[1]) = %#x \n", &(pa[1]));
+	printf(" ++pa = %#x \n", ++pa);
+}
