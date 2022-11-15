9e1099cb13ec77a4b4c69c954f26ae4d5de1e29b
Courtney Maynard <cbmaynard@wm.edu>
2022-11-15 14:52:28 -0500

    Add print statements for clarity

--- ASSIGNMENT_10/play.c
+++ ASSIGNMENT_10/play.c
@@ -15,21 +15,25 @@ void two (char *pa )
 	printf(" addr of pointer param = %#x \n", &pa);
 	printf(" addr of (pa[0]) = %#x \n", &(pa[0]));
 	printf(" addr of (pa[1]) = %#x \n", &(pa[1]));
-	printf(" ++pa = %#x \n", ++pa);
+	printf(" ++pa = %#x \n\n", ++pa);
 }
 
 main() 
 {
 	char ca[24];
 	char *pa;
-
+	
+	printf("output of one() with ca as input to function:\n");
 	one(ca);
+	printf("output of two() with pa as input to function:\n");
 	two(pa);
 	
+	printf("output of one()  with ga as input to function:\n");
 	one(ga);
+	printf("output of two()  with ga as input to function: \n");
 	two(ga);
 	
-	printf(" addr of global array = %#x \n", &ga);
-        printf(" addr (ga[0]) = %#x \n", &(ga[0]));
-        printf( " addr (ga[1]) = %#x \n\n", &(ga[1]));
+	printf("addr of global array = %#x \n", &ga);
+        printf("addr (ga[0]) = %#x \n", &(ga[0]));
+        printf( "addr (ga[1]) = %#x \n\n", &(ga[1]));
 }
